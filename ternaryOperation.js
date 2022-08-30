d216f352edd5fa2268b80c446082d76621240583
Kerem Can CELEPKOLU <88246848+keremclp@users.noreply.github.com>
2022-08-30 14:05:37 +0300

    Add files via upload

---
+++ ternaryOperation.js
@@ -0,0 +1,23 @@
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!')
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+let favoritePhrase = "Love That!";
+favoritePhrase ? console.log('I love that!') : console.log("I don\'t love that!")
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
